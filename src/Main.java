732f9bc5783f0e4be1549c4910551a06abd29835
TimaTostokov <t1005828@gmail.com>
2023-06-17 13:46:05 +0300

    HomeWork_5 on quick wits

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        Boss Shanks = new Boss(0,0,"NULL");
-        Shanks.setHealth(700);
-        Shanks.setDamage(50);
-        Shanks.setBossDefence("shield");
-        System.out.print(" BossHealth: " + Shanks.getHealth());
-        System.out.print(" BossDamage: " + Shanks.getDamage());
-        System.out.print(" BossDefence: " + Shanks.getBossDefence());
+        Boss shanks = new Boss(0, 0, "NULL");
+        shanks.setHealth(700);
+        shanks.setDamage(50);
+        shanks.setBossDefence("shield");
+        System.out.print(" BossHealth: " + shanks.getHealth() + "   Boss Damage : " + shanks.getDamage() + " Boss Ability : " + shanks.getBossDefence());
+
+        System.out.println("-----------------------------------------------------------");
+
+        Hero[] heroes = createHeroes();
+        for (Hero hero : heroes) {
+            System.out.println(" HeroesHealth = " + hero.getHealth() + " HeroesDamage = " + hero.getDamage() + " HeroesSuperAbility = " + hero.getSupersposobnost());
         }
+
+
     }
+
+    public static Hero[] createHeroes() {
+        Hero spider = new Hero(300, 20, "WEB");
+        Hero golem = new Hero(250, 30);
+        Hero iron = new Hero(280, 25, "Steel");
+        Hero[] heroes = {spider, golem, iron};
+        return heroes;
+    }
+}
